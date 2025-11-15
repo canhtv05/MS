e50f8b571175f8174967fd946b75c526d3a3cb4b
canhtv05 <canhtvph51036@gmail.com>
2025-11-15 19:46:39 +0700

    fix: fix web client

--- web-client/views/pages/home/HomPage.tsx
+++ web-client/views/pages/home/HomPage.tsx
@@ -1,16 +1,13 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/animate-ui/components/buttons/button";
-import HeaderHomeLayout from "@/views/layouts/home/HeaderHomeLayout";
-import HomeBanner from "@/views/layouts/home/components/HomeBanner";
-import Live2DCanvas from "@/views/layouts/home/components/Live2DCanvas";
+import HeaderHomeLayout from '@/views/layouts/home/HeaderHomeLayout';
+import HomeBanner from '@/views/layouts/home/components/HomeBanner';
 
 const HomPage = () => {
   return (
     <div className="h-[2000px] bg-white">
       <HeaderHomeLayout />
       <HomeBanner />
-      <Live2DCanvas />
     </div>
   );
 };
