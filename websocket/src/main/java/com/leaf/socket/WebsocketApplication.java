8e429a7a8b5ecfffd7e75011f79c510f055843fd
canhtv05 <canhtvph51036@gmail.com>
2026-03-01 15:40:15 +0700

    feat(websocket): add websocket service

---
+++ websocket/src/main/java/com/leaf/socket/WebsocketApplication.java
@@ -0,0 +1,19 @@
+package com.leaf.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = { "com.leaf", "com.leaf.common" })
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure()
+            .filename(".env.dev")
+            .ignoreIfMissing()
+            .load();
+
+        dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+}
---
+++ websocket/src/test/java/com/leaf/notification/WebsocketApplicationTests.java
@@ -0,0 +1,11 @@
+package com.leaf.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
